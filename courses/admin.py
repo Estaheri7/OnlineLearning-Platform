f78748fa051ec94211e9ce01509a04ec36c49e5b
Estaheri7 <estaheri101@gmail.com>
2024-07-30 21:56:45 +0330

    add: added courses app models

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from .models import Category, Course, Module, Lession, Assignment, Submission
 
-# Register your models here.
+
+
+class ModuleInline(admin.TabularInline):
+    model = Module
+    extra = 1
+
+
+class LessionInline(admin.TabularInline):
+    model = Lession
+    extra = 1
+
+
+class AssignmentInline(admin.TabularInline):
+    model = Assignment
+    extra = 1
+
+
+class SubmissionInline(admin.TabularInline):
+    model = Submission
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent', 'created_time', 'updated_time')
+    search_fields = ('title',)
+    list_filter = ('parent',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'instructor', 'created_time', 'updated_time')
+    search_fields = ('title', 'description', 'instructor__username')
+    list_filter = ('instructor', 'category')
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'course', 'created_time', 'updated_time')
+    search_fields = ('title',)
+    list_filter = ('course',)
+    inlines = [LessionInline, AssignmentInline]
+
+
+@admin.register(Lession)
+class LessionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'module', 'created_time', 'updated_time')
+    search_fields = ('title',)
+    list_filter = ('module',)
+
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'module', 'created_time', 'updated_time', 'due_time')
+    search_fields = ('title',)
+    list_filter = ('module',)
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ('student', 'assignment', 'submitted_at')
+    search_fields = ('student__username', 'assignment__title')
+    list_filter = ('assignment', 'student')
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,99 @@
+import os
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    avatar = models.ImageField(blank=True, upload_to='categories/')
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
+class Course(models.Model):
+    category = models.ManyToManyField(Category, blank=True, related_name='courses')
+    title = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(blank=True, null=True)
+    instructor = models.ForeignKey('users.CustomUser', related_name='instructed_courses', on_delete=models.CASCADE)
+    students = models.ManyToManyField('users.CustomUser', related_name='enrolled_courses', blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'courses'
+        verbose_name = 'course'
+        verbose_name_plural = 'courses'
+
+
+class Module(models.Model):
+    course = models.ForeignKey(Course, blank=False, null=False, related_name='modules', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'modules'
+        verbose_name = 'module'
+        verbose_name_plural = 'modules'
+
+
+def get_upload_to_lession(instance, filename):
+    module = instance.module
+    course = module.course
+    return os.path.join('courses', str(course.id), str(module.id),  'lessions', filename)
+
+
+class Lession(models.Model):
+    module = models.ForeignKey(Module, blank=False, null=False, related_name='lessions', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    content = models.FileField(blank=True, null=True, upload_to=get_upload_to_lession)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'lessions'
+        verbose_name = 'lession'
+        verbose_name_plural = 'lessions'
+
+
+def get_upload_to_assignments(instance, filename):
+    module = instance.module
+    course = module.course
+    return os.path.join('courses', str(course.id), str(module.id),  'assignments', filename)
+
+
+class Assignment(models.Model):
+    module = models.ForeignKey(Module, blank=False, null=False, related_name='assignments', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=False, null=False)
+    content = models.FileField(blank=True, null=True, upload_to=get_upload_to_assignments)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    due_time = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'assignments'
+        verbose_name = 'assignment'
+        verbose_name_plural = 'assignments'
+
+
+def get_upload_to_assignments(instance, filename):
+    student = instance.student
+    assignment = instance.assignment
+    return os.path.join('assignments', str(student.id), str(assignment.id), 'submissions', filename)
+
+
+class Submission(models.Model):
+    student = models.ForeignKey('users.CustomUser', blank=False, null=False, related_name='submissions', on_delete=models.CASCADE)
+    assignment = models.ForeignKey(Assignment, blank=False, null=False, on_delete=models.CASCADE)
+    content = models.FileField(blank=True, null=True, upload_to=get_upload_to_assignments)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'submissions'
+        verbose_name = 'submission'
+        verbose_name_plural = 'submissions'
