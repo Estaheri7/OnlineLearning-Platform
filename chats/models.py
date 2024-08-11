442b58b77b3523b541178543bf49caa3978c4fdd
erfansalman <estaheri101@gmail.com>
2024-08-11 20:54:08 +0330

    add: added Message model in chat models

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(CustomUser, related_name='messages_received', on_delete=models.CASCADE)
+    content = models.TextField()
+    message_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender} to {self.receiver}: {self.content[:10]}'
